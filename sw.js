5c9906b4306f5bb41ed55dd4d27aac1fd2547474
tneophytou22 <tneophytou22@gmail.com>
2026-09-03 22:35:54 +0300

    Restore V11 master service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE='violin-ai-v11';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='violin-ai-v10';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
