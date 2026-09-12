017138a73d3778def734410172edc9ac122a884d
tneophytou22 <tneophytou22@gmail.com>
2026-09-12 18:11:02 +0300

    V15 foundation: expose application services

---
+++ v15/index.js
@@ -0,0 +1,8 @@
+export * from './domain/models.js';
+export { InMemoryRepository } from './repository/in-memory-repository.js';
+export { StudentService } from './services/student-service.js';
+export { TermService } from './services/term-service.js';
+export { LessonService } from './services/lesson-service.js';
+export { ProgrammeService } from './services/programme-service.js';
+export { HomeworkService } from './services/homework-service.js';
+export * from './curriculum/registry.js';
