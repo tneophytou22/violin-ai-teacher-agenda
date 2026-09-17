4ee292cfd40e4d48fe808712d751c42ec55c881f
tneophytou22 <tneophytou22@gmail.com>
2026-09-17 15:30:11 +0300

    export teacher agenda controller

--- v15/index.js
+++ v15/index.js
@@ -11,6 +11,7 @@ export { TeacherTermService } from './services/teacher-term-service.js';
 export { WeeklyProgrammeService } from './services/weekly-programme-service.js';
 export { LessonProgrammeService } from './services/lesson-programme-service.js';
 export { TeacherAgendaViewModel } from './ui/teacher-agenda-view-model.js';
+export { TeacherAgendaController } from './ui/teacher-agenda-controller.js';
 export * from './curriculum/registry.js';
 export * from './curriculum/v1-registration.js';
 export * from './tktl/teacher-unit-card.js';
