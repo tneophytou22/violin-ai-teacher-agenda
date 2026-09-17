6ec36747e6d6f7c4c668360ebee25a26bd364936
tneophytou22 <tneophytou22@gmail.com>
2026-09-17 15:08:10 +0300

    chore(v15): export teacher agenda view model

--- v15/index.js
+++ v15/index.js
@@ -10,6 +10,7 @@ export { HomeworkService } from './services/homework-service.js';
 export { TeacherTermService } from './services/teacher-term-service.js';
 export { WeeklyProgrammeService } from './services/weekly-programme-service.js';
 export { LessonProgrammeService } from './services/lesson-programme-service.js';
+export { TeacherAgendaViewModel } from './ui/teacher-agenda-view-model.js';
 export * from './curriculum/registry.js';
 export * from './curriculum/v1-registration.js';
 export * from './tktl/teacher-unit-card.js';
