2488b3073b97c8ead1885d4c1dd45c5111c1cd8c
tneophytou22 <tneophytou22@gmail.com>
2026-09-17 22:04:14 +0300

    add V15 UI exports

---
+++ v15/ui/index.js
@@ -0,0 +1,4 @@
+export { TeacherAgendaController } from './teacher-agenda-controller.js';
+export { TeacherAgendaViewModel } from './teacher-agenda-view-model.js';
+export { TeacherAgendaShell } from './teacher-agenda-shell.js';
+export { createTeacherAgendaApp } from './teacher-agenda-app.js';
