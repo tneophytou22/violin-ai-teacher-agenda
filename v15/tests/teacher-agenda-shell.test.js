895b88cf4b7bb29e08c16937854cdf78380dccaa
tneophytou22 <tneophytou22@gmail.com>
2026-09-24 08:09:05 +0300

    V15 Phase 60: fix shell test intelligence import

--- v15/tests/teacher-agenda-shell.test.js
+++ v15/tests/teacher-agenda-shell.test.js
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { InMemoryRepository, StudentService, TermService, TeacherTermService, WeeklyProgrammeService, LessonService, LessonProgrammeService } from '../index.js';
+import { InMemoryRepository, StudentService, TermService, TeacherTermService, WeeklyProgrammeService, LessonService, LessonProgrammeService, StudentIntelligenceService } from '../index.js';
 import { TeacherAgendaViewModel } from '../ui/teacher-agenda-view-model.js';
 import { TeacherAgendaController, localDateString } from '../ui/teacher-agenda-controller.js';
 import { TeacherAgendaShell } from '../ui/teacher-agenda-shell.js';
